699c85051fd361dc5e1198de8be5be9f04dbe3e5
Marcio Vinicius <marcio.campos@outlook.com>
2018-05-27 21:10:54 -0300

    Initial commit

---
+++ app/src/main/java/br/com/marcio/meuimc/MainActivity.java
@@ -0,0 +1,42 @@
+package br.com.marcio.meuimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void CalcularIMC(View view){
+        TextView txtIMC = findViewById(R.id.txtImcId);
+        TextView txtDescricaoIMC = findViewById(R.id.txtDescricaoImcId);
+
+        EditText edtPeso = findViewById(R.id.edtPesoId);
+        EditText edtAltura = findViewById(R.id.edtAlturaId);
+
+        double peso = Double.valueOf(edtPeso.getText().toString());
+        double altura = Double.parseDouble(edtAltura.getText().toString())/100;
+        double imc = Math.round(peso/(altura*altura));
+        txtIMC.setText(String.valueOf(imc));
+
+        String descricao = informaClassificacaoIMC(imc);
+
+        txtDescricaoIMC.setText(String.valueOf(descricao));
+
+    }
+
+    public String informaClassificacaoIMC(double imc){
+
+        String descricao = "Peso normal";
+
+        return descricao;
+
+    }
+}
---
+++ app/src/main/java/br/com/marcio/meuimc/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package br.com.marcio.meuimc;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashScreenActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
